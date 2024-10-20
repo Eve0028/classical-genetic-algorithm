c9798ea80f863621e1ab6bd75f777322e7ce9ba8
Anna Zielińska <lananahb@gmail.com>
2024-10-20 12:48:04 +0200

    generation improvement

--- source/population/population.py
+++ source/population/population.py
@@ -1,11 +1,11 @@
 from source.population.individual import Individual
 from source.utils.binary_utils import BinaryUtils
 
-
 class Population:
-    def __init__(self, population_size, number_of_genes, precision, start_interval, end_interval):
-        self.population_size = population_size
-        self.individuals = self.generate_individuals(population_size, number_of_genes, precision, start_interval,
+    def __init__(self, population_size, number_of_genes, precision, start_interval, end_interval, generate=True):
+        self.individuals = []
+        if generate:
+            self.individuals = self.generate_individuals(population_size, number_of_genes, precision, start_interval,
                                                      end_interval)
 
     def generate_individuals(self, population_size, number_of_genes, precision, start_interval, end_interval):
