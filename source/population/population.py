5ae5526941e710425f3a99f81708b4a7e3a909a3
Eve0028 <62481375+Eve0028@users.noreply.github.com>
2024-10-23 18:40:03 +0200

    doc: Add docstrings and small corrections

--- source/population/population.py
+++ source/population/population.py
@@ -1,17 +1,30 @@
 from source.population.individual import Individual
 from source.utils.binary_utils import BinaryUtils
 
+
 class Population:
-    def __init__(self, population_size,  number_of_chromosomes, precision, start_interval, end_interval, generate=True):
-        self.individuals = []
+    def __init__(self, population_size: int, number_of_chromosomes: int, precision: int, start_interval: float,
+                 end_interval: float, generate: bool = True):
+        """
+        Initializes a Population with the given parameters.
+
+        :param population_size: Number of individuals in the population.
+        :param number_of_chromosomes: Number of chromosomes per individual.
+        :param precision: Precision for gene values.
+        :param start_interval: Start of the interval for gene values.
+        :param end_interval: End of the interval for gene values.
+        :param generate: Whether to generate individuals upon initialization.
+        """
         self.population_size = population_size
+        self.number_of_chromosomes = number_of_chromosomes
+        self.precision = precision
+        self.start_interval = start_interval
+        self.end_interval = end_interval
+        self.individuals = []
         if generate:
-          self.individuals = self.generate_individuals(population_size,  number_of_chromosomes, precision, start_interval,
-                                                     end_interval)
+            self.individuals = self.generate_individuals()
 
-    def generate_individuals(self, population_size, number_of_chromosomes, precision, start_interval, end_interval):
-        number_of_genes = BinaryUtils.get_binary_length(start_interval, end_interval, precision)
-        individuals = [Individual(number_of_chromosomes, number_of_genes, start_interval, end_interval, generate=True) for _ in
-                       range(population_size)]
-
-        return individuals
+    def generate_individuals(self) -> list:
+        number_of_genes = BinaryUtils.get_binary_length(self.start_interval, self.end_interval, self.precision)
+        return [Individual(self.number_of_chromosomes, number_of_genes, self.start_interval, self.end_interval,
+                           generate=True) for _ in range(self.population_size)]
--- source/utils/binary_utils.py
+++ source/utils/binary_utils.py
@@ -3,12 +3,26 @@ import numpy as np
 
 class BinaryUtils:
     @staticmethod
-    def get_binary_length(start_interval, end_interval, precision):
+    def get_binary_length(start_interval: float, end_interval: float, precision: int) -> int:
+        """
+        Calculates the binary length required to represent a number in a given range with specified precision.
+
+        :param start_interval: Start of the interval.
+        :param end_interval: End of the interval.
+        :param precision: Number of decimal places.
+        :return: Rounded up binary length.
+        """
         number_of_intervals = (end_interval - start_interval) * pow(10, precision)
         binary_length = np.log2(number_of_intervals)
         rounded_up_binary_length = np.ceil(binary_length)
         return int(rounded_up_binary_length)
 
     @staticmethod
-    def decode_number(binary_array):
+    def decode_number(binary_array: np.ndarray) -> int:
+        """
+        Decodes a binary array to a decimal number.
+
+        :param binary_array: Binary array.
+        :return: Decoded decimal number.
+        """
         return np.sum(binary_array * (2 ** np.arange(binary_array.size)[::-1]))
