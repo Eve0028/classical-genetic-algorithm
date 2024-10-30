276c7c5b94f3319062128bcae0c10ea0e0698057
Anna Zielińska <lananahb@gmail.com>
2024-10-30 23:54:59 +0100

    fixed crossover uniform

--- source/crossover/uniform_crossover.py
+++ source/crossover/uniform_crossover.py
@@ -2,6 +2,7 @@ from source.crossover.crossover import Crossover
 from source.population.individual import Individual
 import numpy as np
 from typing import List
+import copy
 
 class UniformCrossover(Crossover):
 
@@ -9,16 +10,21 @@ class UniformCrossover(Crossover):
         self.crossover_probability = crossover_probability
 
     def cross(self, population: List[Individual]) -> list[Individual]:
-        for individual in population:
-            for chromosome in individual.chromosomes :
-                self.cross_genes(chromosome)
+        if len(population) % 2 != 0:
+            self.cross_individuals(copy.deepcopy(population[len(population) - 2]), population[len(population) - 1])
 
+        for i in range(0, len(population) - 1, 2):
+            self.cross_individuals(population[i], population[i + 1])
         return population
 
-    def cross_genes(self, chromosome):
-        for gene_index in range(len(chromosome)):
+    def cross_individuals(self, individual1: Individual, individual2: Individual):
+        for chromosome1, chromosome2 in zip(individual1.chromosomes, individual2.chromosomes):
+            self.cross_genes(chromosome1, chromosome2)
+
+    def cross_genes(self, chromosome1, chromosome2):
+        for gene_index in range(len(chromosome1)):
             not_crossover = np.random.rand()
             if self.crossover_probability < not_crossover:
                 continue
-            # XOR
-            chromosome[gene_index] ^= 1
+            chromosome1[gene_index], chromosome2[gene_index] = chromosome2[gene_index], chromosome1[gene_index]
+
--- tests/crossover/test_uniform_crossover.py
+++ tests/crossover/test_uniform_crossover.py
@@ -6,11 +6,11 @@ def test_uniform_crossover1():
     uniform_crossover = UniformCrossover()
     uniform_crossover.crossover_probability=1
     expected_individual1_before = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
 
     expected_individual2_before = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                                             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
     individual1 = Individual(0, 0, 0, 0, False)
     individual1.chromosomes= expected_individual1_before.copy()
@@ -21,6 +21,105 @@ def test_uniform_crossover1():
 
     np.testing.assert_array_equal(population[0].chromosomes,  expected_individual2_before)
     np.testing.assert_array_equal(population[1].chromosomes, expected_individual1_before)
+    assert len(population) == 2
+
+def test_uniform_crossover3():
+    uniform_crossover = UniformCrossover()
+    uniform_crossover.crossover_probability=1
+    expected_individual1_before = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    expected_individual2_before = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+    individual1 = Individual(0, 0, 0, 0, False)
+    individual1.chromosomes= expected_individual1_before.copy()
+    individual2 = Individual(0, 0, 0, 0, False)
+    individual2.chromosomes= expected_individual2_before.copy()
+
+    population = uniform_crossover.cross([individual1, individual2])
+
+    np.testing.assert_array_equal(population[0].chromosomes,  expected_individual2_before)
+    np.testing.assert_array_equal(population[1].chromosomes, expected_individual1_before)
+    assert len(population) == 2
+
+def test_uniform_crossover4():
+    uniform_crossover = UniformCrossover()
+    uniform_crossover.crossover_probability=1
+    expected_individual1_before = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    expected_individual2_before = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+    individual1 = Individual(0, 0, 0, 0, False)
+    individual1.chromosomes= expected_individual1_before.copy()
+    individual2 = Individual(0, 0, 0, 0, False)
+    individual2.chromosomes= expected_individual2_before.copy()
+
+    population = uniform_crossover.cross([individual1, individual2])
+
+    np.testing.assert_array_equal(population[0].chromosomes,  expected_individual2_before)
+    np.testing.assert_array_equal(population[1].chromosomes, expected_individual1_before)
+    assert len(population) == 2
+
+def test_uniform_crossover5():
+    uniform_crossover = UniformCrossover()
+    uniform_crossover.crossover_probability=1
+    expected_individual1_before = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    expected_individual2_before = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+    individual1 = Individual(0, 0, 0, 0, False)
+    individual1.chromosomes= expected_individual1_before.copy()
+    individual2 = Individual(0, 0, 0, 0, False)
+    individual2.chromosomes= expected_individual2_before.copy()
+
+    expected_individual3_before = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    expected_individual4_before = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+    individual3 = Individual(0, 0, 0, 0, False)
+    individual3.chromosomes= expected_individual3_before.copy()
+    individual4 = Individual(0, 0, 0, 0, False)
+    individual4.chromosomes= expected_individual4_before.copy()
+
+    population = uniform_crossover.cross([individual1, individual2, individual3, individual4])
+
+    np.testing.assert_array_equal(population[0].chromosomes,  expected_individual2_before)
+    np.testing.assert_array_equal(population[1].chromosomes, expected_individual1_before)
+    np.testing.assert_array_equal(population[2].chromosomes,  expected_individual4_before)
+    np.testing.assert_array_equal(population[3].chromosomes, expected_individual3_before)
+    assert len(population) == 4
+
+
+def test_uniform_crossover_when_crossover_should_not_happen():
+    uniform_crossover = UniformCrossover()
+    uniform_crossover.crossover_probability=0
+    expected_individual1_before = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    expected_individual2_before = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+    individual1 = Individual(0, 0, 0, 0, False)
+    individual1.chromosomes= expected_individual1_before.copy()
+    individual2 = Individual(0, 0, 0, 0, False)
+    individual2.chromosomes= expected_individual2_before.copy()
+
+    population = uniform_crossover.cross([individual1, individual2])
+
+    np.testing.assert_array_equal(population[0].chromosomes,  expected_individual1_before)
+    np.testing.assert_array_equal(population[1].chromosomes, expected_individual2_before)
     assert len(population) == 2
 
 def test_uniform_crossover2():
@@ -35,5 +134,34 @@ def test_uniform_crossover2():
 
     population = uniform_crossover.cross([individual1, individual2])
 
-    np.testing.assert_array_equal(population[0].chromosomes,  np.array([[1, 0, 1, 0, 0, 1, 0, 1, 0, 1]]))
-    np.testing.assert_array_equal(population[1].chromosomes, np.array([[1, 1, 0, 0, 1, 0, 1, 0, 1, 1]]))
+    np.testing.assert_array_equal(population[0].chromosomes,  np.array([[0, 0, 1, 1, 0, 1, 0, 1, 0, 0]]))
+    np.testing.assert_array_equal(population[1].chromosomes, np.array([[0, 1, 0, 1, 1, 0, 1, 0, 1, 0]]))
+
+
+def test_uniform_crossover6():
+    uniform_crossover = UniformCrossover()
+    uniform_crossover.crossover_probability=1
+    expected_individual1_before = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    expected_individual2_before = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    expected_individual3_before = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+    individual1 = Individual(0, 0, 0, 0, False)
+    individual1.chromosomes= expected_individual1_before.copy()
+    individual2 = Individual(0, 0, 0, 0, False)
+    individual2.chromosomes= expected_individual2_before.copy()
+    individual3 = Individual(0, 0, 0, 0, False)
+    individual3.chromosomes= expected_individual3_before.copy()
+
+    population = uniform_crossover.cross([individual1, individual2, individual3])
+
+    np.testing.assert_array_equal(population[0].chromosomes,  expected_individual2_before)
+    np.testing.assert_array_equal(population[1].chromosomes, expected_individual1_before)
+    np.testing.assert_array_equal(population[2].chromosomes, expected_individual2_before)
+    assert len(population) == 3
