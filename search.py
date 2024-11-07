06e2b377376ee3a35d61498ffc166bd17edcb761
Eve0028 <62481375+Eve0028@users.noreply.github.com>
2024-11-07 14:51:20 +0100

    refactor: Parameters range for happy cat

--- search.py
+++ search.py
@@ -34,21 +34,27 @@ def objective(trial: optuna.trial.Trial, function: callable, num_variables: int,
     trial.set_user_attr('precision', precision)
 
     population_size = trial.suggest_int('population_size', 100, 1000)
-    num_generations = trial.suggest_int('num_generations', 100, 1000)
+    num_generations = trial.suggest_int('num_generations', 500, 2000)
 
-    selection_strategy_type = trial.suggest_categorical('selection_strategy', ['ROULETTE', 'TOURNAMENT', 'BEST'])
+    # selection_strategy_type = trial.suggest_categorical('selection_strategy', ['ROULETTE', 'TOURNAMENT', 'BEST'])
+    selection_strategy_type = trial.suggest_categorical('selection_strategy', ['TOURNAMENT'])
     selection_size = trial.suggest_int('selection_size', 10,
                                        population_size) if selection_strategy_type != 'TOURNAMENT' else None
     tournament_size = trial.suggest_int('tournament_size', 2,
-                                        population_size // 5) if selection_strategy_type == 'TOURNAMENT' else None
+                                        3) if selection_strategy_type == 'TOURNAMENT' else None
     selection_strategy = SelectionStrategyEnum[selection_strategy_type].value(
         selection_size if selection_size else tournament_size)
 
-    elite_strategy = trial.suggest_categorical('elite_strategy', [True, False])
-    elite_size = trial.suggest_int('elite_size', 1, population_size // 5) if elite_strategy else 0
+    # elite_strategy = trial.suggest_categorical('elite_strategy', [True, False])
+    elite_strategy = trial.suggest_categorical('elite_strategy', [True])
+    # elite_size = trial.suggest_int('elite_size', 1, population_size // 5) if elite_strategy else 0
+    elite_size = trial.suggest_int('elite_size', population_size // 100, population_size // 20) if elite_strategy else 0
 
-    crossover_strategy_type = trial.suggest_categorical('crossover_strategy', ['POINT', 'DISCRETE', 'UNIFORM'])
-    crossover_probability = trial.suggest_float('crossover_probability', 0.5, 1.0)
+    # crossover_strategy_type = trial.suggest_categorical('crossover_strategy', ['POINT', 'DISCRETE', 'UNIFORM'])
+    crossover_strategy_type = trial.suggest_categorical('crossover_strategy', ['POINT', 'UNIFORM'])
+    crossover_probability = trial.suggest_float('crossover_probability', 0.8, 0.9)
+    # intersection_number = trial.suggest_int('intersection_number', 1, 2) if crossover_strategy_type in [
+    #     'POINT'] else None
     intersection_number = trial.suggest_int('intersection_number', 1, 2) if crossover_strategy_type in [
         'POINT'] else None
     crossover_size = population_size - elite_size
@@ -59,14 +65,15 @@ def objective(trial: optuna.trial.Trial, function: callable, num_variables: int,
         crossover_strategy = CrossoverStrategyEnum[crossover_strategy_type].value(crossover_size,
                                                                                   crossover_probability)
 
-    mutation_strategy_type = trial.suggest_categorical('mutation_strategy', ['ONE_POINT', 'TWO_POINT', 'BOUNDARY'])
-    mutation_probability = trial.suggest_float('mutation_probability', 0.01, 0.5)
+    # mutation_strategy_type = trial.suggest_categorical('mutation_strategy', ['ONE_POINT', 'TWO_POINT', 'BOUNDARY'])
+    mutation_strategy_type = trial.suggest_categorical('mutation_strategy', ['ONE_POINT', 'TWO_POINT'])
+    mutation_probability = trial.suggest_float('mutation_probability', 0.01, 0.1)
     mutation_strategy = MutationStrategyEnum[mutation_strategy_type].value(mutation_probability)
 
     inversion = None
     is_inversion = trial.suggest_categorical('inversion', [True, False])
     if is_inversion:
-        inversion_probability = trial.suggest_float('inversion_probability', 0.01, 0.5)
+        inversion_probability = trial.suggest_float('inversion_probability', 0.05, 0.3)
         inversion = Inversion(inversion_probability)
 
     logger.info(f"Evolution initialized with the following parameters:\n"
@@ -196,7 +203,7 @@ def run_study(database_url: str, function: callable, function_name: str, num_var
     """
     pruner = MedianPruner(n_startup_trials=5, n_warmup_steps=3, interval_steps=1)
     study = optuna.create_study(
-        study_name=f'{function_name}__var_{num_variable}__prec_{precision}',
+        study_name=f'{function_name}__var_{num_variable}__prec_{precision}__specific_args',
         direction='minimize',
         storage=database_url,
         load_if_exists=True,
@@ -209,7 +216,7 @@ def run_study(database_url: str, function: callable, function_name: str, num_var
     partial_objective = partial(objective, function=function, num_variables=num_variable,
                                 start_interval=start_interval, end_interval=end_interval,
                                 search_minimum=True, precision=precision)
-    study.optimize(partial_objective, n_trials=30)
+    study.optimize(partial_objective, n_trials=15)
 
     print(
         f"Best parameters for {function_name}, num variables {num_variable}, precision {precision}: {study.best_params}")
